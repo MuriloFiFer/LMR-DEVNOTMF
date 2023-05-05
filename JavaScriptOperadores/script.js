a257aa33e0005c5c2d49fa900cbae293fff6b73a
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-05-04 21:25:10 -0300

    04/05/23

--- JavaScriptOperadores/script.js
+++ JavaScriptOperadores/script.js
@@ -1,3 +1,3 @@
 let NOME;
-    NOME = prompt('Entre com seu nome:','Digite-0 aqui');
+    NOME = prompt('Entre com seu nome:','Digite-o aqui');
     document.write('Oi '+ NOME + ' esteja a vontade')
