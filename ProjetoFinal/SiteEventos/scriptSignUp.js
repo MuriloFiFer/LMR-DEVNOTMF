bea30b594dfce53ef0451aa37a377737e4e0cc57
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-06-14 00:37:08 -0300

    13/06/23 - atualizado 00:30h

--- ProjetoFinal/SiteEventos/scriptSignUp.js
+++ ProjetoFinal/SiteEventos/scriptSignUp.js
@@ -36,20 +36,19 @@ function Enviar() {
       return false;
     }
   
-    var evento = document.getElementsByName("NomeEvent")[0].value;
-    var quantidadeIngressos = document.getElementsByName("quantIngressos")[0].value;
-  
+
  
   
     //  exibir os dados no console
     console.log("Nome: " + nome);
     console.log("Endereço: " + endereco);
     console.log("Email: " + email);
-    console.log("Evento: " + evento);
-    console.log("Quantidade de Ingressos: " + quantidadeIngressos);
+
   
     // Se chegou até aqui, todos os campos foram preenchidos corretamente
-    alert("Formulário enviado com sucesso!");
+    alert("Cadastro realizado com sucesso!");
+    alert( "Seu nome:"+ nome);
+    alert("seu email:"+email)
     return true;
   }
   
